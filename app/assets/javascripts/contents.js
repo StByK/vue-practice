84665ed27d4ef0327e18d4ae575df1a5996f7704
StByK <miz07og18kag@gmail.com>
2019-11-12 10:57:35 +0900

    method

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   new Vue({
     el: '#app',
     data: {
-      message: "Hello World"
+      message: "Hello World",
+      number: 0,
+      url: "https://google.com",
+    },
+    methods: {
+      countUp: function() {
+        this.number += 1
+      }
     }
   })
 })
