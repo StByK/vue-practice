5e1560107e09796a5f3c71e4ed43fd2c1c8c7f1c
StByK <miz07og18kag@gmail.com>
2019-11-13 17:52:07 +0900

    mosemove

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -6,10 +6,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
       message: "Hello World",
       number: 0,
       url: "https://google.com",
+      x: 0,
+      y: 0
     },
     methods: {
       countUp: function() {
         this.number += 1
+      },
+      mousePosition: function(event) {
+        this.x = event.clientX;
+        this.y = event.clientY;
       }
     }
   })
